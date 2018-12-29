05ea1ed18ba74f99fa4f165f56189745a2658e5b
Zainab-Abdulmanea <zezo.saz@gmail.com>
2018-12-29 23:42:31 +0300

    Handle the code sent back from the callback method

--- application.py
+++ application.py
@@ -1,15 +1,23 @@
 # Web Server modules
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 # Database modules
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Provider, Course
 
-# OAuth modules to generate unique session token
+# Library to generate unique session token
 from flask import session as login_session
 import random
 import string
+
+# Import OAuth libraries to handle the code sent back from callback method
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
 
 app = Flask(__name__)
 
@@ -18,9 +26,13 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# declare CLIENT_ID using client_secrets file
+CLIENT_ID = json.loads(open('client_secrets.json',
+                            'r').read())['web']['client_id']
+
 
 # ------------------- LOGIN -------------------------
-# view login page
+# Make nti-forgery state token and view login page
 @app.route('/login')
 def viewLogin():
     # generate a random anti-forgery state token mixed of letters and digits
@@ -31,6 +43,94 @@ def viewLogin():
     login_session['state'] = state_token
     # render login page
     return render_template('login.html', STATE=state_token)
+
+
+# Create G-connect page that accepts one-time code
+# and handle the code sent back from the callback method 
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # ensure the user is making the request by validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # obtain one-time code
+    code = request.data
+
+    try:
+        # exchange the authorization code into a credentials object which contains the access_token
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # ensure the access token is valid
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' %
+           access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # if there was an error in the access token info, abort
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # ensure that the access token is used for the intended user
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # ensure that the access token's client ID is valid for this app
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # check is the user is already logged in 
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(
+            json.dumps('You are already connected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # store the access token in the login session 
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
 
 
 # ------------------- App Pages -------------------------
@@ -76,7 +176,7 @@ def viewCourse(provider_name, course_name):
 @app.route('/provider/course/new/', methods=['GET', 'POST'])
 def newCourse():
     if request.method == 'POST':
-        # get the selected provider from the dropdown list
+        # obtain the selected provider from the dropdown list
         courseProvider = session.query(Provider).filter_by(
             id=request.form.get('selected-provider')).one()
         # create a new provider course
@@ -102,7 +202,7 @@ def editCourse(provider_name, course_name):
     # Get the course you want to update
     course = session.query(Course).filter_by(name=course_name).one()
     if request.method == 'POST':
-        # get the selected provider from the dropdown list
+        # obtain the selected provider from the dropdown list
         courseProvider = session.query(Provider).filter_by(
             id=request.form.get('selected-provider')).one()
 
